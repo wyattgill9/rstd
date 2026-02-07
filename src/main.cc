22515a1f5c65962439d078a957979a0f24380463
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2026-02-07 15:11:10 -0800

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,42 +1,26 @@
-#include "schema.hh"
 #include "tsdb.hh"
 
 #include <cstring>
 
-using namespace schema;
-
 struct Vec3 {
-    f64 x;
-    f64 y;
-    f64 z;
+    f64 x, y, z;
 };
 
-
 auto main() -> i32 {
-    TypeRegistry reg;
+	TSDB db {4};
 
-    // Create the Schema
-    auto vec3_handle = reg.register_struct({
-        {"x", TypeRegistry::F64},
-        {"y", TypeRegistry::F64},
-        {"z", TypeRegistry::F64},
-    });
+	auto vec3_handle = db.register_struct(
+    "Vec3", {
+		{"x", TSDB::F64},
+		{"y", TSDB::F64},
+		{"z", TSDB::F64},
+	});
 
-    // Init DB
-    TSDB db { reg };
+	db.insert(Vec3 { .x = 1, .y = 1, .z = 1}, vec3_handle);
 
-    // Insert some type into the DB
-    auto vec3 = Vec3 {
-        .x = 1,
-        .y = 1,
-        .z = 1
-    };
-    db.insert(reinterpret_cast<std::byte*>(&vec3), vec3_handle); // maybe make it just pass in (vec3, vec3_handle)
-       
-    // Query that type
-    Vec3 new_vec;
-    db.query_all_into(vec3_handle, reinterpret_cast<std::byte*>(&new_vec));
+	Vec3 new_vec;
+	db.query_first(new_vec, vec3_handle);
     std::println("new_vec {{.x = {}, .y = {}, .z = {}}}", new_vec.x, new_vec.y, new_vec.z);
-    
-    return 0;
+
+	return 0;
 }
